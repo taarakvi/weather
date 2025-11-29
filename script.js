0201ba645971e0980515db38f97f48ea5a7bfa66
taarakvi <taarakvi777@gmail.com>
2025-11-29 23:44:01 +0530

    weather

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ let button = document.querySelector(".searchB")
     let outputL5 = document.getElementById("outputL5")
     let outputL6 = document.getElementById("outputL6")
     let outputL7 = document.getElementById("outputL7")
-
-
-
+    let outputL8 = document.getElementById("outputL8")
+    let outputL9 = document.getElementById("outputL9")
+    let outputL10 = document.getElementById("outputL10")
     let tempC = document.getElementById("temp-c")
 
 async function getData(cityName){
@@ -26,30 +26,6 @@ async function getData(cityName){
     }    
 }
 
-// button.addEventListener("click",async()=>{
-//     let value = input.value;
-//     let result =await getData(value);
-//     console.log(result)
-//     outputL1.innerText = `${result.location.name}`;
-//     outputL2.innerText = `${result.location.country},`;
-//     outputL3.innerText = `${result.location.region},`;
-//     outputL4.innerText = `${result.location.tz_id},`;
-//     tempC.innerText = `${result.current.temp_c}°c | ${result.current.temp_f}°f`;
-//     outputL5.innerText = `${result.current.wind_kph}k/h`;
-//     outputL6.innerText = `${result.current.humidity}%`
-//     outputL7.innerText = `${result.current.precip_in}%`
-
-//     // console.log(result.location.country)
-//     // console.log(result.location.name)
-//     // console.log(result.location.region) 
-//     // console.log(result.location.localtime)
-//     // console.log("temp-c : ",result.current.temp_c)
-//     // console.log("temp-f : ",result.current.temp_f)
-//     // console.log("wind : ",result.current.wind_kph)
-//     // console.log("humidity : ",result.current.humidity)
-//     // console.log("precipitation : ",result.current.precip_in)
-
-// })
 async function some(){
     let value = input.value;
     let result =await getData(value);
@@ -62,28 +38,50 @@ async function some(){
     outputL5.innerText = `${result.current.wind_kph}k/h`;
     outputL6.innerText = `${result.current.humidity}%`
     outputL7.innerText = `${result.current.precip_in}%`
+    outputL8.innerText = `${result.current.cloud}`
+    outputL9.innerText = `${result.current.dewpoint_c}°c | ${result.current.dewpoint_f}°f`
+    outputL10.innerText = `${result.current.pressure_in}`
 
-    console.log(result.location.country)
-    console.log(result.location.name)
-    console.log(result.location.region) 
-    console.log(result.location.localtime)
-    console.log("temp-c : ",result.current.temp_c)
-    console.log("temp-f : ",result.current.temp_f)
-    console.log("wind : ",result.current.wind_kph)
-    console.log("humidity : ",result.current.humidity)
-    console.log("precipitation : ",result.current.precip_in)
-
+    // console.log(result.location.country)
+    // console.log(result.location.name)
+    // console.log(result.location.region) 
+    // console.log(result.location.localtime)
+    // console.log("temp-c : ",result.current.temp_c)
+    // console.log("temp-f : ",result.current.temp_f)
+    // console.log("wind : ",result.current.wind_kph)
+    // console.log("humidity : ",result.current.humidity)
+    // console.log("precipitation : ",result.current.precip_in)
+    let date = new Date()
+    console.log(date)
+    let x = date.getHours()
+    console.log(x)
+    let mImg = document.getElementById("m-img")
+    let nImg = document.getElementById("n-img")
+    for(let i = x; i<=x; i++){
+    if(x>=18){
+        console.log("night")
+        mImg.style.display = "none"
+        nImg.style.display = "block"
+    }else{
+        console.log("morning")
+        mImg.style.display = "block"
+        nImg.style.display = "none"
+    }
+    }
+    
 } 
+// for enternal key
 button.addEventListener("click",async()=>{
    some();
 })
+// for external keywords
 input.addEventListener("keydown",(e)=>{
     if(e.key === "Enter"){
         some();
     }
 })
 
-
+// for clear text
 let cutB = document.querySelector(".cutB")
 cutB.addEventListener("click",()=>{
     input.value = "";
